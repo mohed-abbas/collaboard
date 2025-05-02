f058ef15900e4c96f6c9a823e46815d3adeee069
Wilson San <wilson.san57@gmail.com>
2025-05-02 15:48:36 +0200

    added Task model + migration + some relation

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,4 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model {}
+class Project extends Model
+{
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+}
