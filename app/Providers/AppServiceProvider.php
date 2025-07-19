b66fd7644d2adc787bc27b7443785a6b42ac35c9
Mohed Abbas <mohed332@gmail.com>
2025-07-19 22:59:49 +0200

    Update app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 use App\Models\Project;
 use App\Policies\ProjectPolicy;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,6 +30,26 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
+        }
+
+        if ($this->app->environment('production')) {
+            VerifyEmail::createUrlUsing(function ($notifiable) {
+                $expiration = Carbon::now()
+                    ->addMinutes(config('auth.verification.expire', 60));
+
+                // 1) Build the normal signed URL (absolute with http:// or https://)
+                $signed = URL::temporarySignedRoute(
+                    'verification.verify',
+                    $expiration,
+                    [
+                        'id' => $notifiable->getKey(),
+                        'hash' => sha1($notifiable->getEmailForVerification()),
+                    ]
+                );
+
+                // 2) Strip off the protocol, leaving "//domain/path?query"
+                return preg_replace('#^https?:#', '', $signed);
+            });
         }
     }
 }
